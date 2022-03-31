4945279d1ad94c92decbc6e21aa00a372694b731
Evgeny Novoselov <zkodru@gmail.com>
2022-03-31 11:42:39 +0300

    Поправил по checkstyle

--- src/main/java/com/novoselov1/sample/githubactions/Sample.java
+++ src/main/java/com/novoselov1/sample/githubactions/Sample.java
@@ -1,7 +1,14 @@
 package com.novoselov1.sample.githubactions;
 
 public class Sample {
-    public int sum(int a, int b) {
+    /**
+     * Sum method.
+     *
+     * @param a First arg.
+     * @param b Second arg.
+     * @return Sum args.
+     */
+    public int sum(final int a, final int b) {
         return a + b;
     }
 }
